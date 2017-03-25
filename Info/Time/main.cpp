829814f7d6e85b37f7acb6aa54a4cfd55388e99e
natade-jp <natade3@gmail.com>
2017-03-26 00:47:31 +0900

    現在時刻を取得

---
+++ Info/Time/main.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <time.h>
+
+int showDate(void) {
+	time_t timer;
+	struct tm *local = NULL;
+	
+	// 現在時刻を取得
+	if(time(&timer) == (time_t)-1) {
+		return 1;
+	}
+	
+	// 内部に持っている変数のため free は不要
+	local = localtime(&timer);
+	
+	{
+		int iYear   = local->tm_year + 1900;
+		int iMonth  = local->tm_mon  + 1;
+		int iDay    = local->tm_mday;
+		int iHour   = local->tm_hour;
+		int iMinute = local->tm_min;
+		int iSecond = local->tm_sec;
+		
+		printf("%04d/%02d/%02d %02d:%02d:%02d\n",
+			iYear, iMonth, iDay, iHour, iMinute, iSecond );
+	}
+	
+	return 0;
+}
+
+int main(void){
+	showDate();
+	return 0;
+}
