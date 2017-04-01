b6b3fb11c8be7691642739a1480c58f95a492cae
natade-jp <natade3@gmail.com>
2017-04-01 19:20:52 +0900

    syslog の sh版サンプル

---
+++ System/SyslogSH/logger.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# syslog の設定ファイルの場所は、次のうちのどちらか
+# /etc/syslog.conf, /etc/rsyslog.conf
+LOGSETTINGFILE=/etc/rsyslog.conf
+echo "settings = " $LOGSETTINGFILE
+
+# (/\w+)+/messages のワードを調べて、ファイルパスを調べる
+# o ... マッチする部分を調べる
+# e ... 正規表現を使用する
+LOGFILE=`cat /etc/rsyslog.conf | grep -oe "\(/\w\+\)\+/messages"`
+echo "logfile = " $LOGFILE
+
+# バックグラウンドでログファイルを表示させる
+tail -f $LOGFILE &
+
+# ログファイルを書き込みする
+# -p syslog.info 優先度の設定 (info のほかにも、debug や err 等がある)
+# -t タグ名をともに記録する
+logger -p syslog.info -t APPLICATION_NAME "data"
+
+# 少し待つ
+sleep 3
+
+#終了
+killall tail
