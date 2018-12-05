c173aef82def94448adbb75eebe0c608d12496f4
natade-jp <natade3@gmail.com>
2018-12-05 23:22:07 +0900

    オーバーロードの試験を追加

--- Overloading/main.cpp
+++ Overloading/main.cpp
@@ -5,11 +5,15 @@ void Func(void) {
 }
 
 void Func(unsigned char x) {
-	printf("unsigned char[%02X]\n", x);
+	printf("unsigned char[0x%02X]\n", x);
 }
 
 void Func(int x) {
-	printf("int[%08X]\n", x);
+	printf("int[0x%08X]\n", x);
+}
+
+void Func(unsigned long long x) {
+	printf("unsigned long long[0x%llx]\n", x);
 }
 
 void Func(float x) {
@@ -21,13 +25,21 @@ void Func(double x) {
 }
 
 int main() {
-	Func();
+	printf("* unsigned char *\n");
 	Func((unsigned char)0xFF);
 	Func((unsigned char)0x01 + (unsigned char)0x01);
 	Func((unsigned char)((unsigned char)0x01 + (unsigned char)0x01));
-	Func(1);
+	
+	printf("* unsigned long long *\n");
+	Func((unsigned long long)0xFF);
+	Func((unsigned long long)0x01 + (unsigned long long)0x01);
+	Func((unsigned long long)((unsigned long long)0x01 + (unsigned long long)0x01));
+	
+	printf("* float *\n");
 	Func(1.0f + 1.0f);
 	Func(1.4e-45f * 0.0000001f);
+	
+	printf("* double *\n");
 	Func(1.4e-45 * 0.0000001);
 	return 0;
 }
