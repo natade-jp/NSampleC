859d3404752b408e0ee0e3e88d86f5730ae373f0
natade-jp <natade3@gmail.com>
2019-04-24 00:55:41 +0900

    C言語とC++言語のプログラムを混在させるサンプル

---
+++ System/MixedC++/getone.h
@@ -0,0 +1,15 @@
+#ifndef INCLUDED_GETONE
+#define INCLUDED_GETONE
+
+// C++で使用する場合は、C言語の記載方法として認識させる
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int getone( void );
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ System/MixedC++/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "getone.h"
+
+int main(void){
+	
+	printf("getone = %d\n", getone() );
+	
+	return 0;
+}
+
+/*
+# ./exe
+getone = 1
+*/
